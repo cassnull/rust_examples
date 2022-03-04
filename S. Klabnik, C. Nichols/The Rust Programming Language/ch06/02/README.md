0495d7113ee2643b184b565cf7370e1bac25a746
cassnull <cassnull@mail.ru>
2022-03-04 08:09:14 +0300

    S. Klabnik, C. Nichols/The Rust Programming Language/ch06/02/binding_catchall_and_placeholder

--- S. Klabnik, C. Nichols/The Rust Programming Language/ch06/02/README.md
+++ S. Klabnik, C. Nichols/The Rust Programming Language/ch06/02/README.md
@@ -1,2 +1,3 @@
 - [enums_and_pattern_matching](enums_and_pattern_matching/README.md)
 - [matching_with_option](matching_with_option/README.md)
+- [binding_catchall_and_placeholder](binding_catchall_and_placeholder/README.md)
---
+++ S. Klabnik, C. Nichols/The Rust Programming Language/ch06/02/binding_catchall_and_placeholder/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
+    }
+}
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(_num_spaces: u8) {}
