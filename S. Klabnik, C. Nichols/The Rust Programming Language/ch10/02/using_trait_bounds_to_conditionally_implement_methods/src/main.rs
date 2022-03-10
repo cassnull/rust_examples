da05964ba2e3b419579c524f3445b0787c76422c
cassnull <cassnull@mail.ru>
2022-03-10 20:22:22 +0100

    S. Klabnik, C. Nichols/The Rust Programming Language/ch10/02/using_trait_bounds_to_conditionally_implement_methods

---
+++ S. Klabnik, C. Nichols/The Rust Programming Language/ch10/02/using_trait_bounds_to_conditionally_implement_methods/src/main.rs
@@ -0,0 +1,29 @@
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
+fn main() {
+    let pair = Pair::new(16, 292);
+    pair.cmp_display();
+
+    let _s = 3.to_string();
+}
