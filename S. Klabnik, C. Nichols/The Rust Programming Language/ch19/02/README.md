cefd5c1f01cb4bd73203e668ed60dc208f80ddc5
cassnull <cassnull@mail.ru>
2022-04-02 13:24:06 +0300

    S. Klabnik, C. Nichols/The Rust Programming Language/ch19/02/default_generic_type_parameters_and_operator_overloading

--- S. Klabnik, C. Nichols/The Rust Programming Language/ch19/02/README.md
+++ S. Klabnik, C. Nichols/The Rust Programming Language/ch19/02/README.md
@@ -1,1 +1,2 @@
 - [specifying_placeholder_types_in_trait_definitions_with_associated_types](specifying_placeholder_types_in_trait_definitions_with_associated_types/README.md)
+- [default_generic_type_parameters_and_operator_overloading](default_generic_type_parameters_and_operator_overloading/README.md)
---
+++ S. Klabnik, C. Nichols/The Rust Programming Language/ch19/02/default_generic_type_parameters_and_operator_overloading/src/main.rs
@@ -0,0 +1,91 @@
+use std::ops::Add;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Millimeters(f32);
+
+#[derive(Debug, PartialEq)]
+struct Meters(f32);
+
+impl Add for Millimeters {
+    type Output = Millimeters;
+    fn add(self, rhs: Self) -> Self::Output {
+        Millimeters(self.0 + rhs.0)
+    }
+}
+
+impl Add for Meters {
+    type Output = Meters;
+    fn add(self, rhs: Self) -> Self::Output {
+        Meters(self.0 + rhs.0)
+    }
+}
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000.0))
+    }
+}
+
+impl Add<Millimeters> for Meters {
+    type Output = Meters;
+    fn add(self, rhs: Millimeters) -> Self::Output {
+        Meters(self.0 + rhs.0 / 1000.0)
+    }
+}
+
+fn main() {
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+
+    println!("34mm + 2m = {:?}", Millimeters(34.0) + Meters(2.0));
+    println!("1m + 255mm = {:?}", Meters(1.0) + Millimeters(255.0));
+
+    println!("25m + 75m = {:?}", Meters(25.0) + Meters(75.0));
+    println!("25mm + 75mm = {:?}", Millimeters(25.0) + Millimeters(75.0));
+}
+
+#[cfg(test)]
+mod test {
+    use super::{Meters, Millimeters};
+
+    #[test]
+    fn add_meters() {
+        assert_eq!(Meters(3.), Meters(1.0) + Meters(2.0));
+    }
+
+    #[test]
+    fn add_millimeters() {
+        assert_eq!(Millimeters(30.0), Millimeters(10.0) + Millimeters(20.0));
+    }
+
+    #[test]
+    fn add_mm_to_m() {
+        assert_eq!(Meters(5.0) + Millimeters(350.0), Meters(5.35));
+    }
+
+    #[test]
+    fn add_m_to_mm() {
+        assert_eq!(Millimeters(3500.0) + Meters(2.0), Millimeters(5500.0));
+    }
+}
