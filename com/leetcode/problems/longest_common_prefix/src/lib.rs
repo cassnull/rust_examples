2e8df2a5a789c8a000e481ab4c419f4354eaac91
cassnull <cassnull@mail.ru>
2022-07-06 21:51:37 +0300

    com/leetcode/problems/longest_common_prefix

--- com/leetcode/problems/longest_common_prefix/src/lib.rs
+++ com/leetcode/problems/longest_common_prefix/src/lib.rs
@@ -2,7 +2,7 @@ pub struct Solution;
 
 impl Solution {
     pub fn longest_common_prefix(strs: Vec<String>) -> String {
-        if strs.len() == 0 {
+        if strs.is_empty() {
             return "".to_string();
         }
         let mut index = 0;
@@ -21,8 +21,58 @@ impl Solution {
         "".to_string()
     }
 
+    pub fn longest_common_prefix_2(strs: Vec<String>) -> String {
+        if strs.is_empty() {
+            return "".to_string();
+        }
+
+        fn func(strs: &Vec<String>, l: usize, r: usize) -> String {
+            if l == r {
+                return strs[l].clone();
+            }
+            let mid = (l + r) / 2;
+            let lcp_left: Vec<char> = func(strs, l, mid).chars().collect();
+            let lcp_right: Vec<char> = func(strs, mid + 1, r).chars().collect();
+            let min = lcp_left.len().min(lcp_right.len());
+            for i in 0..min {
+                if lcp_left[i] != lcp_right[i] {
+                    return lcp_left[0..i].iter().collect();
+                }
+            }
+            lcp_left[0..min].iter().collect()
+        }
+
+        func(&strs, 0, strs.len() - 1)
+    }
+
+    pub fn longest_common_prefix_3(strs: Vec<String>) -> String {
+        if strs.is_empty() {
+            return "".to_string();
+        }
+
+        fn func(strs: &[String], len: usize) -> bool {
+            let str1 = strs[0][0..len].to_string();
+            println!("{}", str1);
+            strs.iter().all(|str| str.starts_with(&str1))
+        }
+
+        let min = strs.iter().map(|str| str.len()).min().unwrap();
+
+        let mut low = 0;
+        let mut hight = min;
+        while low <= hight {
+            let middle = (low + hight) / 2;
+            if func(&strs, middle) {
+                low = middle + 1;
+            } else {
+                hight = if middle == 0 { 0 } else { middle - 1 };
+            }
+        }
+        strs[0][0..(low + hight) / 2].to_string()
+    }
+
     pub fn longest_common(strs: Vec<String>) -> String {
-        if strs.len() == 0 {
+        if strs.is_empty() {
             return "".to_string();
         }
         let mut index = 0;
@@ -58,12 +108,44 @@ mod tests {
             ]),
             "fl"
         );
+        assert_eq!(
+            Solution::longest_common_prefix_2(vec![
+                "flower".to_string(),
+                "flow".to_string(),
+                "flight".to_string()
+            ]),
+            "fl"
+        );
+        assert_eq!(
+            Solution::longest_common_prefix_3(vec![
+                "flower".to_string(),
+                "flow".to_string(),
+                "flight".to_string()
+            ]),
+            "fl"
+        );
     }
 
     #[test]
     fn example_2() {
         assert_eq!(
             Solution::longest_common_prefix(vec![
+                "dog".to_string(),
+                "racecar".to_string(),
+                "car".to_string()
+            ]),
+            ""
+        );
+        assert_eq!(
+            Solution::longest_common_prefix_2(vec![
+                "dog".to_string(),
+                "racecar".to_string(),
+                "car".to_string()
+            ]),
+            ""
+        );
+        assert_eq!(
+            Solution::longest_common_prefix_3(vec![
                 "dog".to_string(),
                 "racecar".to_string(),
                 "car".to_string()
@@ -81,6 +163,35 @@ mod tests {
                 "flight".to_string()
             ]),
             ""
+        );
+        assert_eq!(
+            Solution::longest_common_prefix_2(vec![
+                "reflower".to_string(),
+                "flow".to_string(),
+                "flight".to_string()
+            ]),
+            ""
+        );
+        assert_eq!(
+            Solution::longest_common_prefix_3(vec![
+                "reflower".to_string(),
+                "flow".to_string(),
+                "flight".to_string()
+            ]),
+            ""
+        );
+    }
+
+    #[test]
+    fn example_4() {
+        assert_eq!(Solution::longest_common_prefix(vec!["a".to_string()]), "a");
+        assert_eq!(
+            Solution::longest_common_prefix_2(vec!["a".to_string()]),
+            "a"
+        );
+        assert_eq!(
+            Solution::longest_common_prefix_3(vec!["a".to_string()]),
+            "a"
         );
     }
 }
